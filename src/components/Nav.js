47b7aff783117cf67ca70594a47f5334e72844ff
jdbohac <jeffrey.bohac@gmail.com>
2023-04-01 15:31:29 -0400

    added some styling to nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,13 @@
 const Nav = (props)=>{
     return(
+    <>
+    <div className="header"><img src="https://i.imgur.com/Nvxelxh.png" alt="" />
         <div id="nav">
             <h3 id='home' onClick={props.showHome}>Home</h3>
             <h3 id='profile' onClick={props.showProfile}>Profile</h3>
         </div>
+        </div>
+        </>
     )
 }
 
